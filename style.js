124e092b3a407fb43c17c173a42ab0f2c9aec91e
leesoojin96 <69374300+leesoojin96@users.noreply.github.com>
2020-08-13 11:39:33 +0900

    Add files via upload
    2020년8월12일자

---
+++ style.js
@@ -0,0 +1,10 @@
+
+  let btn = document.getElementById('light');
+  btn.addEventListener('click',function () {
+    document.getElementById('target').className = 'light';
+  })
+
+  let btn2 = document.getElementById('pink');
+  btn2.addEventListener('click',function () {
+    document.getElementById('target').className = 'pink';
+  })
